f0bf0935697d502f52c3388985549d5d48253bf7
ash9431 <achraf9431@gmail.com>
2019-01-17 20:56:07 +0100

    add models information

--- shops/models.py
+++ shops/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Shops(models.Model):
+    name = models.CharField(max_length=70, blank=True, null=True)
+    distance = models.IntegerField(blank=True, null=True)
+
+
+class User(AbstractUser):
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', ]
+    # override User.email:
+    email = models.EmailField(unique=True, blank=False, null=False)
+    shops = models.ManyToManyField(Shops)
+
+
+
+
+
