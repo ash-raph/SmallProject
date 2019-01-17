16725e00e3fa8d7467f62b9ed6b363c679970ed7
ash9431 <achraf9431@gmail.com>
2019-01-17 22:57:06 +0100

    add reaction

--- shops/urls.py
+++ shops/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from shops.views import ShopsListView
+from shops.views import ShopsListView, ReactView
 
 urlpatterns = [
-    url(r'^shops/', ShopsListView.as_view(), name="list_of_shops"),
+    url(r'^shops/$', ShopsListView.as_view(), name="list_of_shops"),
+    url(r'^shops/(?P<pk>\d+)/react/$', ReactView.as_view(), name="like_dislike"),
 ]
--- shops/views.py
+++ shops/views.py
@@ -1,7 +1,22 @@
 from django.views.generic import ListView
+from django.views.generic.base import View
+from django.shortcuts import redirect, reverse
 from shops.models import Shops
 
 
 class ShopsListView(ListView):
     model = Shops
     template_name = 'shops_list.html'
+
+
+class ReactView(View):
+    def post(self, request, **kwargs):
+        shop_instance = Shops.objects.filter(pk=kwargs.get('pk')).first()
+
+        if 'Like' in request.POST:
+            self.request.user.shops.add(shop_instance)
+
+        elif 'Dislike' in request.POST:
+            self.request.user.shops.remove(shop_instance)
+
+        return redirect(reverse('list_of_shops'))
